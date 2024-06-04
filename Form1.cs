e32902ea0e7d983e6bb5806bf92a07e659d7fc7a
ricardoalzugaray <131892627+ricardoalzugaray@users.noreply.github.com>
2024-06-04 19:51:14 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Modelo : Form
+    {
+        public Modelo()
+        {
+            InitializeComponent();
+        }
+
+        static double acumulador;
+        static int contador;
+
+        private void btnAgregarNota_Click(object sender, EventArgs e)
+        {
+
+        }
+        static double CalcularPromedio()
+        {
+            double promedio = 0;
+            if (contador > 0)
+                promedio = acumulador/contador;
+            return promedio;
+        }
+        static void RegistrarNota(double nota)
+        {
+            acumulador += nota;
+            contador++;
+        }
+
+        private void btnAgregarNota_Click_1(object sender, EventArgs e)
+        {
+            double nota = Convert.ToDouble(tbIngresarNota.Text);
+            RegistrarNota(nota);
+            tbIngresarNota.Clear();
+            lbContador.Text = contador.ToString();
+        }
+
+        private void btnConsultarPromedio_Click(object sender, EventArgs e)
+        {
+            double promedio = CalcularPromedio();
+            lbMostrarPromedio.Text = promedio.ToString();
+            
+        }
+    }
+}
